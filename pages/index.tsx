77194810500ef58d7ef2093159f09d81f339ec93
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-11-11 00:06:18 +0530

    Is Excetuable Workng

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,21 @@ import * as Web3 from '@solana/web3.js'
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
-
+  const [isExecutable, setIsExecutable] = useState(false);
   const addressSubmittedHandler = (address: string) => {
     try {
       setAddress(address)
       const key = new Web3.PublicKey(address)
       const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      
       connection.getBalance(key).then(balance => {
         setBalance(balance / Web3.LAMPORTS_PER_SOL)
+      })
+
+      connection.getAccountInfo(key).then(info => {
+        console.log(info);
+        
+        setIsExecutable(info?.executable ?? false);
       })
     } catch (error) {
       setAddress('')
@@ -22,6 +29,8 @@ const Home: NextPage = () => {
       alert(error)
     }
   }
+
+  
 
   return (
     <div className={styles.App}>
@@ -32,6 +41,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`is Exectucale? ${isExecutable?"YES":"NO"}`}</p>
       </header>
     </div>
   )
